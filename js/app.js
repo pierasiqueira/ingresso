d2d5e541a03f626b501b34caa8c1dd198d56ca8d
pierasiqueira <pierasiqueira@gmail.com>
2025-04-10 19:44:04 -0300

    finalizado

--- js/app.js
+++ js/app.js
@@ -4,16 +4,42 @@ function comprar() {
 
     if(tipo.value == 'pista') {
         comprarPista(qtd);
+    } else if (tipo.value == 'superior') {
+        comprarSuperior(qtd);
+    } else {
+        comprarInferior(qtd);
     }
 }
 
 function comprarPista(qtd) {
     let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
     if (qtd > qtdPista) {
-        alert('Quantidade indisponível de ingressos Pista');
+        alert('Quantidade indisponível de ingressos da Pista');
     } else {
         qtdPista = qtdPista - qtd;
         document.getElementById('qtd-pista').textContent = qtdPista;
+        alert('Compra realizada com sucesso!');
+    }
+}
+
+function comprarSuperior(qtd) {
+    let qtdSuperior = parseInt(document.getElementById('qtd-superior').textContent);
+    if (qtd > qtdSuperior) {
+        alert('Quantidade indisponível de ingressos de Cadeira superior');
+    } else {
+        qtdSuperior = qtdSuperior - qtd;
+        document.getElementById('qtd-superior').textContent = qtdSuperior;
+        alert('Compra realizada com sucesso!');
+    }
+}
+
+function comprarInferior(qtd) {
+    let qtdInferior = parseInt(document.getElementById('qtd-inferior').textContent);
+    if (qtd > qtdInferior) {
+        alert('Quantidade indisponível de ingressos de Cadeira inferior');
+    } else {
+        qtdInferior = qtdInferior - qtd;
+        document.getElementById('qtd-inferior').textContent = qtdInferior;
         alert('Compra realizada com sucesso!');
     }
 }
